b77afe4416b41a1ce5237f33a315bf793ff7f7f7
dicky199624 <leeshingyan123456@gmail.com>
2023-08-31 16:55:29 +0800

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # safety_detect
 
-A new Flutter project.
+firebase flow:
+
+There are two methods for sending notifications: `sendNotification` and `sendNotification2`.
+
+The first method, `sendNotification`, is used to send warning notifications to users. It is called when the detection personnel swipes the image to the right in the `safety_detect` function. The method requires the image URL and the mobile token as parameters.
+
+The second method, `sendNotification2`, is used to send apology notifications to users. It is called when the detection personnel swipes the image to the right and then clicks the return button in the `safety_detect` function. (The previous image must be swiped to the right to invoke this method, and a parameter value is changed to true when the image is swiped to the right. This method will not be called if the parameter value is false, even if the return button is clicked.)
+
+The agent's mobile receives images:
+The program directly retrieves all images from Firebase storage and displays them on the agent's mobile.
+
+Determining if the image is swiped to the right, indicating danger:
+When the image is swiped to the right, the current image name is obtained. Then, it checks if the current image name contains "a" or "b". (In Firebase storage, "a" corresponds to the first user's uploaded image, and "b" corresponds to the second user's uploaded image.) If the image is identified as "a", the current image URL is obtained, and the current image URL and the mobile token of the first user's mobile (the mobile token is obtained using the "fcm_notification" software and manually copied to the `safety_detect` function) are passed to the `sendNotification` method to send a notification to the user's mobile. (Firebase's messaging service identifies the receiving mobile for broadcast notifications based on the mobile token of the user's mobile, so the mobile token needs to be obtained.)
+
+Return button:
+The return button only sends an apology notification if the previous image is swiped to the right. Otherwise, it simply switches back to the previous image. When the previous image is swiped to the right and the detection personnel click the return button, the `sendNotification2` method is called, and the image (which is manually copied into Firebase storage) and the mobile token (obtained manually using the "fcm_notification" software) are passed as parameters to the method.
+
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -104,8 +104,8 @@ void sendNotification(String deviceToken, String image) async {
 void sendNotification2(String deviceToken, String image) async {
   // Create the apologize notification message
   var notification = {
-    'title': 'Sorry is our mistake!',
-    'body': 'You baby is safety~',
+    'title': 'Please check on Dicky!',
+    'body': 'Our AI suggests that he might be in danger~',
     'image':image,
   };
 
@@ -126,7 +126,7 @@ void sendNotification2(String deviceToken, String image) async {
     url,
     headers: headers,
     body: jsonEncode(message),
-  );
+ );
 }
 
 class _DetectPage extends State<DetectPage> {
@@ -171,11 +171,14 @@ class _DetectPage extends State<DetectPage> {
         child: Stack(
           children: [
             Positioned.fill(
+              //swipe animation control
               child: AppinioSwiper(
                 backgroundCardsCount: 3,
                 swipeOptions:
+                //make the swipe just only can slide to right or left
                     const AppinioSwipeOptions.only(right: true, left: true),
                 unlimitedUnswipe: true,
+                //make the animation look like pan slide
                 maxAngle: 1,
                 controller: controller,
                 onSwiping: (AppinioSwiperDirection direction) {
@@ -189,7 +192,8 @@ class _DetectPage extends State<DetectPage> {
                 ),
                 cardsCount: imageUrls.length,
                 cardsBuilder: (BuildContext context, int index) {
-                  return Image.network(
+                  //load the picture form firebase storage to swiper
+                  return Image.network( 
                     imageUrls[index],
                     fit: BoxFit.cover,
                   );
@@ -224,14 +228,12 @@ class _DetectPage extends State<DetectPage> {
 
     if (lastSwiped.startsWith('a')) {
       childNode = "returnA";
-      String image = "https://firebasestorage.googleapis.com/v0/b/message-test-push-futter.appspot.com/o/sorry.jpg?alt=media&token=beb2874c-5872-4980-8497-b77091c7fd4d";
       String deviceToken = 'd3OobwZbS8qEOuSLDWp1uQ:APA91bF_8YEw9m3gJDEGrbj3qz4rb97p8qNcNGr_lE0-aNgDmBoIr1vbw0XlTz9J7EY3uBmV4YT6xl41f-4PZiAcltCPFPIv9cbmBzwarPs6XmYLkGFl8ZYuTde8D5Y2RFqqVldEPMwj';
-      sendNotification2(deviceToken,image);
+      sendNotification2(deviceToken,'https://firebasestorage.googleapis.com/v0/b/message-test-push-futter.appspot.com/o/sorry.jpg?alt=media&token=f8030c57-c345-445f-8803-da21527c8114');
     } else if (lastSwiped.startsWith('b')) {
       childNode = "returnB";
-      String image = "https://firebasestorage.googleapis.com/v0/b/message-test-push-futter.appspot.com/o/sorry.jpg?alt=media&token=beb2874c-5872-4980-8497-b77091c7fd4d";
       String deviceToken = 'fxmp4ZbNTIutNK3oTD9nbE:APA91bFJ2tBnNOfLOAcXg-TzExJiwfLPywHQ06qYZpj3byDWMXxas7DzgTrOfgTFWcvNIFLnjv8cPgxwT797-5r69CqHMWvLF4QqIdCLPG5TDqgggXorRv1aNK7C1NDB7ljc4Coe2-XI';
-      sendNotification2(deviceToken,image);
+      sendNotification2(deviceToken,'https://firebasestorage.googleapis.com/v0/b/message-test-push-futter.appspot.com/o/sorry.jpg?alt=media&token=f8030c57-c345-445f-8803-da21527c8114');
     } else {
       return;
     }
@@ -246,50 +248,73 @@ class _DetectPage extends State<DetectPage> {
     }
   }
 
-  void _swipe(int index, AppinioSwiperDirection direction) {
+  // This function handles swipe actions. It takes two parameters: an index and a direction.
+void _swipe(int index, AppinioSwiperDirection direction) {
+    // If the swipe is to the right, then set the lastSwipeRight variable to true.
+    // Otherwise, set it to false.
     if (direction == AppinioSwiperDirection.right) {
       lastSwipeRight = true;
     } else {
       lastSwipeRight = false;
     }
-    // Adjust the index if necessary
+
+    // Adjust the index by subtracting 1.
     int adjustedIndex = index - 1;
 
-    // Perform operations based on the swipe direction
+    // Perform operations based on the swipe direction.
     if (direction == AppinioSwiperDirection.right) {
-      // Determine the child node to use based on the image name
+      // Determine the child node to use based on the image name.
+      // Split the URL of the image at the adjustedIndex and take the last part (the image name).
       String imageName = imageUrls[adjustedIndex]
           .split('/')
-          .last; // Extract the image name from the URL
+          .last; 
+      
       String childNode;
+      // If the image name starts with 'a', set the childNode to "Parent Phone A".
+      // Then, set the value at childNode in the database to the image URL.
       if (imageName.startsWith('a')) {
-        childNode = "Phone A";
+        childNode = "Parent Phone A";
         _database.ref().child(childNode).set(imageUrls[adjustedIndex]);
-      } else if (imageName.startsWith('b')) {
-        childNode = "Phone B";
+      } 
+      // If the image name starts with 'b', set the childNode to "Parent Phone B".
+      // Then, set the value at childNode in the database to the image URL.
+      else if (imageName.startsWith('b')) {
+        childNode = "Parent Phone B";
         _database.ref().child(childNode).set(imageUrls[adjustedIndex]);
-      } else {
+      } 
+      // If the image name does not start with 'a' or 'b', return from the function without doing anything.
+      else {
         return;
       }
 
-      // Remember last swiped image
+      // Remember the last image that was swiped.
       lastSwiped = imageName;
-    }
   }
+ }
 }
 
+// This is a custom widget extending the StatelessWidget class in Flutter. 
+// StatelessWidget is a widget that describes part of the user interface which can depend on configuration but doesn't need to maintain state.
 class ReturnButton extends StatelessWidget {
+  // This is a callback function to be called when the button is tapped.
   final VoidCallback onTap;
+  // This is a widget to be displayed as a child of this widget.
   final Widget child;
 
+  // This is a constructor for the ReturnButton widget. 
+  // It takes two required parameters: a callback function and a child widget. 
+  // It also takes an optional key parameter, which is used by Flutter to efficiently update widgets.
   const ReturnButton({
     required this.onTap,
     required this.child,
     Key? key,
   }) : super(key: key);
 
+  // The build method describes the part of the user interface represented by this widget.
   @override
   Widget build(BuildContext context) {
+    // GestureDetector is a non-visual widget that provides gesture recognition. 
+    // Here it is used to detect a tap gesture and call the provided onTap callback when the gesture is recognized.
     return GestureDetector(
       onTap: onTap,
       child: child,
@@ -297,9 +322,13 @@ class ReturnButton extends StatelessWidget {
   }
 }
 
+// unswipeButton is a function that returns a ReturnButton widget.
+// It takes a callback function as a parameter, which will be executed when the button is pressed.
 Widget unswipeButton(VoidCallback onBackButtonPressed) {
   return ReturnButton(
     onTap: onBackButtonPressed,
+    // The child of the ReturnButton is a Container widget, which is a convenient way to create a visual element with padding, margins, borders, and background color.
+    // Here, the Container is given a height, width, and alignment, and is decorated as a circular shape with a semi-opaque red color.
     child: Container(
       height: 105,
       width: 110,
@@ -313,6 +342,6 @@ Widget unswipeButton(VoidCallback onBackButtonPressed) {
         color: Colors.white,
         size: 110,
       ),
-),
-);
+    ),
+  );
 }
